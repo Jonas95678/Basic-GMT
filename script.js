9a70d6aeb7d98d0330480f7634d7bc04c19ebbdf
aaairam <magildaceria@gmail.com>
2024-03-14 14:56:34 +0800

    header 100%

---
+++ script.js
@@ -0,0 +1,15 @@
+const burgir = document.querySelector(".burgir__menu") 
+const header = document.querySelector(".header__nav")
+burgir.addEventListener("click", () =>{
+    burgir.classList.toggle("open");
+    header.classList.toggle("show");
+})
+
+
+
+
+
+
+// const = declare variables
+// document = whole project
+//.addEventListener = action
